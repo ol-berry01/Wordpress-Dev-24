3a09689ee656cfaf6ce4e44a6036ea08f3cdae3a
Oliver Berry <info@littlemonsterdigital.co.uk>
2024-05-10 11:11:57 +0100

    Fix Google font loading issue

--- inlcudes/front/enqueue.php
+++ inlcudes/front/enqueue.php
@@ -1,9 +1,20 @@
 <?php
     
     function wp_dev24_enqueue() {
-        wp_register_style( 'wp_dev24_rubik_and_pacifico', 'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap' );
-        wp_register_style( 'wp_dev24_bootstrap_icons', get_theme_file_uri( 'assets/bootstrap-icons/bootstrap-icons.css' ) );
-        wp_register_style( 'wp_dev24_theme', get_theme_file_uri( 'assets/public/index.css' ) );
+        wp_register_style( 
+            'wp_dev24_rubik_and_pacifico', 
+            'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap',
+            [],
+            null 
+        );
+        wp_register_style( 
+            'wp_dev24_bootstrap_icons', 
+            get_theme_file_uri( 'assets/bootstrap-icons/bootstrap-icons.css' ) 
+        );
+        wp_register_style( 
+            'wp_dev24_theme', 
+            get_theme_file_uri( 'assets/public/index.css' ) 
+        );
         
         wp_enqueue_style( 'wp_dev24_rubik_and_pacifico' );
         wp_enqueue_style( 'wp_dev24_bootstrap_icons' );
