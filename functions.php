fb603b573d6f2df0c60e03245d6d076334aaa6dd
Oliver Berry <info@littlemonsterdigital.co.uk>
2024-05-10 11:26:50 +0100

    Add preconnect links to wp_head()

--- functions.php
+++ functions.php
@@ -4,6 +4,8 @@
 
     // Includes
     include( get_theme_file_path( 'inlcudes/front/enqueue.php' ) );
+    include( get_theme_file_path( 'inlcudes/front/head.php' ) );
 
     // Hooks
     add_action( 'wp_enqueue_scripts', 'wp_dev24_enqueue' );
+    add_action( 'wp_head', 'wp_dev24_head', 7 );
---
+++ inlcudes/front/head.php
@@ -0,0 +1,8 @@
+<?php
+    
+    function wp_dev24_head() {
+        ?>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <?php
+    }
