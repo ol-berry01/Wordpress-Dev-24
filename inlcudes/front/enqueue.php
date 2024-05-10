eeff01610d99be36a660b566c1a93c6774f6ed6a
Oliver Berry <info@littlemonsterdigital.co.uk>
2024-05-10 11:07:52 +0100

    Enqueue stylesheets

--- inlcudes/front/enqueue.php
+++ inlcudes/front/enqueue.php
@@ -4,4 +4,8 @@
         wp_register_style( 'wp_dev24_rubik_and_pacifico', 'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap' );
         wp_register_style( 'wp_dev24_bootstrap_icons', get_theme_file_uri( 'assets/bootstrap-icons/bootstrap-icons.css' ) );
         wp_register_style( 'wp_dev24_theme', get_theme_file_uri( 'assets/public/index.css' ) );
+        
+        wp_enqueue_style( 'wp_dev24_rubik_and_pacifico' );
+        wp_enqueue_style( 'wp_dev24_bootstrap_icons' );
+        wp_enqueue_style( 'wp_dev24_theme' );
     }
