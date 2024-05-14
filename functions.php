8e5819921104281db1e4822dcddfeb369e5ba36b
Oliver Berry <info@littlemonsterdigital.co.uk>
2024-05-14 21:44:23 +0100

    Set wp_dev24_setup_theme in functions.php
    Include wp_dev24_setup_theme in setup.php

--- functions.php
+++ functions.php
@@ -5,7 +5,9 @@
     // Includes
     include( get_theme_file_path( 'inlcudes/front/enqueue.php' ) );
     include( get_theme_file_path( 'inlcudes/front/head.php' ) );
+    include( get_theme_file_path( 'inlcudes/setup.php' ) );
 
     // Hooks
     add_action( 'wp_enqueue_scripts', 'wp_dev24_enqueue' );
     add_action( 'wp_head', 'wp_dev24_head', 7 );
+    add_action( 'after_setup_theme', 'wp_dev24_setup_theme' ); 
